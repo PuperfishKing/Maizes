bd5cbf0f524c107b0fa44d1d0180c27eb93c800e
BruhSampleText <bob12331rits@gmail.com>
2024-02-22 11:39:03 +0200

    added util functions in main

--- Main.js
+++ Main.js
@@ -10,6 +10,13 @@ function include( file ) {
 	document.getElementsByTagName('head').item(0).appendChild(script);
 }
 
+function vec3( x = 0, y = 0, z = 0 ) {
+	return { x : x, y : y, z : z }
+}
+function getTransform( position = { x : 0, y : 0, z : 0 }, rotation = { x : 0, y : 0, z : 0 } ) { //translateZ( 600px )
+	return `rotateX( ${ rotation.x }deg ) rotateY( ${ rotation.y }deg ) translate3d(${ position.x }px, ${ position.y }px, ${ position.z }px)`
+}
+
 //  File appending
 include( "js/map.js" )
 include( "js/world.js" )
